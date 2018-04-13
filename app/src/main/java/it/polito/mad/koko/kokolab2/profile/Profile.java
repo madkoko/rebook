f3c8caa5d7f515e9da36d98c75682401d86f5708
marcomicera <marco.micera@gmail.com>
2018-04-13 16:07:28 +0200

    Already-signed-up user log-in fix: Profile is now serializable

--- app/src/main/java/it/polito/mad/koko/kokolab2/profile/Profile.java
+++ app/src/main/java/it/polito/mad/koko/kokolab2/profile/Profile.java
@@ -37,51 +37,43 @@ public class Profile {
         this.bio=bio;
     }
 
-    /**
-     *
-     * @param name of user
-     */
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
-        mDatabase.child("users").child(mFirebaseUser.getUid()).child("name").setValue(name);
-
     }
 
-    /**
-     *
-     * @param email of user
-     */
+    public String getEmail() {
+        return email;
+    }
+
     public void setEmail(String email) {
         this.email = email;
-        mDatabase.child("users").child(mFirebaseUser.getUid()).child("email").setValue(email);
     }
 
-    /**
-     *
-     * @param phone of user
-     */
+    public String getPhone() {
+        return phone;
+    }
 
     public void setPhone(String phone) {
         this.phone = phone;
-        mDatabase.child("users").child(mFirebaseUser.getUid()).child("phone").setValue(phone);
     }
 
-    /**
-     *
-     * @param location of user
-     */
+    public String getLocation() {
+        return location;
+    }
+
     public void setLocation(String location) {
         this.location = location;
-        mDatabase.child("users").child(mFirebaseUser.getUid()).child("location").setValue(location);
     }
 
-    /**
-     *
-     * @param bio of user
-     */
+    public String getBio() {
+        return bio;
+    }
+
     public void setBio(String bio) {
         this.bio = bio;
-        mDatabase.child("users").child(mFirebaseUser.getUid()).child("bio").setValue(bio);
     }
-
 }
