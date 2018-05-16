3fed5db3c2502b2bf3761b73c1f013945c02e782
Davide <grimaldellobello>
2018-05-16 23:33:29 +0200

    Chat in showChatActivity retrieving the message in real time!

--- app/src/main/java/it/polito/mad/koko/kokolab3/messaging/ShowChat.java
+++ app/src/main/java/it/polito/mad/koko/kokolab3/messaging/ShowChat.java
@@ -3,23 +3,21 @@ package it.polito.mad.koko.kokolab3.messaging;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
-import android.text.Editable;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.twitter.sdk.android.core.models.Card;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
-import java.util.ArrayList;
 
 import it.polito.mad.koko.kokolab3.R;
 
@@ -28,24 +26,31 @@ public class ShowChat extends AppCompatActivity {
     /**
      * All the messages of the selected chat
      */
-    private ArrayList<Message> messages;
+    //private ArrayList<Message> messages;
     private String chatId;
     private String currentUserID;
     private BaseAdapter baseAdapter;
+    private Query query;
+    private String TAG= "ShowChatActivity";
+
+    private FirebaseListOptions<Message> options;
+    private ListView chatsListView;
+    private FirebaseListAdapter<Message> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_chat);
 
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/database/README.md#using-firebaseui-to-populate-a-listview
 
-
-        messages=(ArrayList<Message>) getIntent().getExtras().get("messages");
+        //messages=(ArrayList<Message>) getIntent().getExtras().get("messages");
         chatId= getIntent().getStringExtra("chatId");
         currentUserID= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        query = FirebaseDatabase.getInstance().getReference().child("chats").child(chatId).child("messages");
 
-        ListView chatsListView = findViewById(R.id.chat_listview);
+
+        chatsListView = findViewById(R.id.chat_listview);
         EditText editText = findViewById(R.id.send_message);
         Button send= findViewById(R.id.send);
 
@@ -56,9 +61,48 @@ public class ShowChat extends AppCompatActivity {
                 //((BaseAdapter) chatsListView.getAdapter()).notifyDataSetChanged();
             }
         });
+        //FirebaseListOptions<Message> for retrieving data from firebase
+        //query is reference
+        options = new FirebaseListOptions.Builder<Message>()
+                .setLayout(R.layout.adapter_show_chat)
+                .setQuery(query, Message.class)
+                .build();
+        Log.d(TAG, String.valueOf(options.getSnapshots()));
+
+        //FirebaseListAdapter for create ListAdapter Ui from firebaseUi
+        adapter = new FirebaseListAdapter<Message>(options) {
+            @Override
+            protected void populateView(View view, Message model, int position) {
+                Log.d(TAG, String.valueOf(model));
+                TextView messageText =  view.findViewById(R.id.message_text);
+
+                messageText.setText(model.getText());
+
+                if(model.getSender().equalsIgnoreCase(currentUserID))
+                    messageText.setGravity(Gravity.RIGHT);
+                else
+                    messageText.setGravity(Gravity.LEFT);
+            }
+        };
+        chatsListView.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
 
         // set the list view to show all the books
-
+        /*
         if (messages != null) {
 
             //Log.d(TAG, "book_list onStart ShowBooks" + book_list.toString());
@@ -99,5 +143,4 @@ public class ShowChat extends AppCompatActivity {
             };
             chatsListView.setAdapter(baseAdapter);
         }
-    }
-}
+        */
